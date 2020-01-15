ad0b9b4280cb66f644793763298fed194e9d0cf5
Paul Burakov <pburakov@spotify.com>
2020-01-15 14:29:42 +0000

    add function to set directory ready for go direnv

--- shell/common.sh
+++ shell/common.sh
@@ -5,3 +5,5 @@ alias weather='curl wttr.in/London'
 
 srv() { host -t srv _spotify-$1._$2.services.gew1.spotify.net; }
 sql() { cloud_sql_proxy --instances=$1=tcp:$2; }
+
+goprep() {echo "export GOPATH=$(pwd)/.go" > .envrc; direnv allow .;}
