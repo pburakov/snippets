52187d7bf397497ab23b842236f167291b48b124
Paul Burakov <pburakov@spotify.com>
2020-05-23 11:19:28 -0400

    goprep -> goinit; idempotent gopath creation

--- shell/common.sh
+++ shell/common.sh
@@ -6,4 +6,4 @@ alias weather='curl wttr.in/London'
 srv() { host -t srv _spotify-$1._$2.services.gew1.spotify.net; }
 sql() { cloud_sql_proxy --instances=$1=tcp:$2; }
 
-goprep() {echo "export GOPATH=$(pwd)/.go" > .envrc; direnv allow .;}
+goinit() {mkdir -p .go; echo "export GOPATH=$(pwd)/.go" > .envrc; direnv allow .;}
