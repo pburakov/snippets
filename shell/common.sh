b44d7cc233e3639e5dc84ea6c50cd38e97b6046c
Paul Burakov <me@pburakov.com>
2021-04-12 10:13:54 -0400

    refresh common.sh

--- shell/common.sh
+++ shell/common.sh
@@ -1,9 +1,5 @@
 # Shortcuts
 alias ll='ls -alh'
 alias dkall='docker kill $(docker ps -q)'
-alias weather='curl wttr.in/London'
-
-srv() { host -t srv _spotify-$1._$2.services.gew1.spotify.net; }
-sql() { cloud_sql_proxy --instances=$1=tcp:$2; }
 
 goinit() {mkdir -p .go; echo "export GOPATH=$(pwd)/.go" > .envrc; direnv allow .;}
